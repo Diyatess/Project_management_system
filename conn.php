3ef6c7d257e494d29a0ee34217b70c38ca21958a
Diya Tes John <118431791+Diyatess@users.noreply.github.com>
2023-10-05 16:24:15 +0530

    connection

---
+++ conn.php
@@ -0,0 +1,11 @@
+<?php
+// Database connection parameters
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "taskmaster";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
