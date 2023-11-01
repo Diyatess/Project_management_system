8e06d26f3c48a9640f2a6db2f832c522161184cd
Diya Tes John <118431791+Diyatess@users.noreply.github.com>
2023-11-02 02:32:04 +0530

    to accept the project

---
+++ accept_request.php
@@ -0,0 +1,16 @@
+<?php
+session_start();
+include('../conn.php'); 
+
+if (isset($_GET['request_id'])) {
+    $requestId = $_GET['request_id'];
+
+    // Update the status of the request to "Accepted" in the database
+    $sql = "UPDATE project_requests SET status = 'Approved' WHERE request_id = $requestId";
+    $conn->query($sql);
+
+    // Redirect back to the Team Lead's profile page
+    header("Location: tdashboard.php");
+    exit;
+}
+?>
