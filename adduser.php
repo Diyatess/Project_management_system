0dc4aad93e1ca3f738fea6043ed717d9e4e52dbd
Diya Tes John <118431791+Diyatess@users.noreply.github.com>
2023-11-12 08:54:34 +0530

    updated add employee in admin page

--- adduser.php
+++ adduser.php
@@ -14,6 +14,48 @@ if ($result) {
     }
 }
 
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
+
+// Load Composer's autoloader
+require '../vendor/autoload.php';
+
+// Function to send a welcome email
+function sendWelcomeEmail($recipientEmail, $recipientName, $verifyToken, $userEmail, $userPassword) {
+    $mail = new PHPMailer(true);
+
+    try {
+        // Server settings
+        $mail->isSMTP();
+        $mail->Host = 'smtp.gmail.com'; // Update with your SMTP server
+        $mail->SMTPAuth = true;
+        $mail->Username = 'taskmasterhub2023@gmail.com'; // Update with your email
+        $mail->Password = 'tgzpeyjxxpjafund'; // Update with your email password
+        $mail->SMTPSecure = 'tls'; // Use TLS
+        $mail->Port = 587; // Port for TLS
+
+        // Recipients
+        $mail->setFrom('taskmasterhub2023@gmail.com', 'TaskMaster Hub'); // Update with your email and name
+        $mail->addAddress($recipientEmail, $recipientName);
+
+        // Content
+        $mail->isHTML(true);
+        $mail->Subject = 'Welcome to Our Website';
+        $mail->Body = 'Hello ' . $recipientName . ',<br>Welcome to our Company!<br>Your login credentials are as follows:<br>' .
+            '<strong>Email:</strong> ' . $userEmail . '<br>' .
+            '<strong>Password:</strong> ' . $userPassword . '<br>' .
+            'Click here to login <a href="http://localhost/Taskmaster/admin/verify.php?token=' . urlencode($verifyToken) . '">Click here to verify your email</a>';
+
+        $mail->send();
+        return true;
+    } catch (Exception $e) {
+        // Log or handle the exception appropriately
+        return false;
+    }
+}
+
+
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $fname = $_POST['fname'];
     $lname = $_POST['lname'];
@@ -26,36 +68,49 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $state = $_POST['state'];
     $pincode = $_POST['pincode'];
     $country = $_POST['country'];
-    $desig_id = $_POST['desig_id']; // This should come from your form, e.g., a dropdown
+    $desig_id = $_POST['desig_id']; 
     $hiredate = $_POST['hiredate'];
+    $password = $_POST['password'];
     $rid = '4';
 
+    // Generate a verification token (you can use any method you prefer)
+    $verifyToken = md5(uniqid(rand(), true));
+
     $selectedDesignation =  $_POST['desig_id']; 
+
     if ($selectedDesignation === "Designer") {
         $desi = "1";
     } elseif ($selectedDesignation === "Codder") {
         $desi = "2";
     } elseif ($selectedDesignation === "Tester") {
         $desi = "3";
-    } 
-    else {
+    } else {
         // Handle an invalid role selection or provide a default department
-        $role = "Other";
+        $desi = "Default"; // You can change this to the appropriate default value
     }
+    
 
     // Generate a random unique MD5 password
-    $random_password = md5(uniqid(rand(), true));
+    $hpassword = md5($password);
 
     // Insert data into the Employee table
-    $sql = "INSERT INTO employee (fname, lname, gender, dob, email, contact, address, city, state, pincode, country, hiredate, password, desig_id)
-            VALUES ('$fname', '$lname', '$gender', '$dob', '$email', '$contact', '$address', '$city', '$state', '$pincode', '$country', '$hiredate', '$random_password', '$desi')";
+    $sql = "INSERT INTO employee (fname, lname, gender, dob, email, contact, address, city, state, pincode, country, hiredate, password, desig_id,verify_token)
+            VALUES ('$fname', '$lname', '$gender', '$dob', '$email', '$contact', '$address', '$city', '$state', '$pincode', '$country', '$hiredate', '$hpassword', '$desi','$verifyToken')";
 //echo $sql;
     if ($conn->query($sql) === TRUE) {
         // Insert data into the teammember table
         $employee_id = $conn->insert_id; // Get the ID of the newly inserted employee
-        $role_id = 4; // Assuming role_id 4 represents a team member
+        $role_id = 4; // entering id as 4 in db
 
-        $teammember_sql = "INSERT INTO teammember (id, mid, task_id, rid) VALUES ($employee_id, NULL, NULL, $rid)";
+        if (sendWelcomeEmail($email, $fname . " " . $lname, $verifyToken, $email, $password)) {
+            // Registration successful. Welcome email sent!
+            $_SESSION['status'] = 'Registration successful. Welcome email sent!';
+        } else {
+            // Registration successful, but there was an issue sending the welcome email.
+            $_SESSION['status'] = 'Registration successful, but there was an issue sending the welcome email.';
+        }
+
+        $teammember_sql = "INSERT INTO teammember (emp_id,task_id, rid) VALUES ($employee_id,  NULL, $rid)";
         if ($conn->query($teammember_sql) === TRUE) {
             echo '<script>alert("Employee data added successfully.");</script>';
             echo '<script>window.location.href = "dashboard.php";</script>';
@@ -69,8 +124,6 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 }
 ?>
 
-?>
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -78,6 +131,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Add Employee</title>
     <link href="../css/style.css" rel="stylesheet" />
+    
     <style>
         /*for header and slider*/
         .slider {
@@ -148,7 +202,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
             display: block;
             margin: 10px 0;
         }
-        input[type="text"], input[type="email"], input[type="date"], input[type="number"] {
+        input[type="text"], input[type="email"], input[type="date"], input[type="number"],input[type="password"] {
             width: 90%;
             padding: 10px;
             border: 1px solid #ccc;
@@ -168,9 +222,27 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
             padding: 10px 20px;
             cursor: pointer;
         }
+        .back-button {
+            background-color: #555;
+            color: #fff;
+            padding: 10px 20px;
+            border: none;
+            border-radius: 5px;
+            cursor: pointer;
+            text-decoration: none;
+            display: inline-block;
+            margin-top: 20px;
+        }
+        .back-button:hover {
+            background-color: #777;
+        }
     </style>
 </head>
-<body>
+<body><a href="dashboard.php" class="back-button" style="float: right;">Back</a>
+<a class="navbar-brand" href="../index.php">
+        <img src="../images/logo.png" alt="" />
+        <span> TaskMasters Hub</span>
+    </a>
     <div class="container">
         <h1>Add Employee</h1>
         <form method="POST" action="" onsubmit="return validateForm()">
@@ -191,6 +263,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
             <label for="dob">Date of Birth:</label>
             <input type="date" id="dob" name="dob" required>
+            <span id="dob-validation" style="color: red;"></span>
 
             <label for="email">Email:</label>
             <input type="email" id="email" name="email" required>
@@ -211,103 +284,28 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
             <label for="pincode">Pincode:</label>
             <input type="text" id="pincode" name="pincode">
+            <div id="pincode-validation" style="color: red;"></div>
 
             <label for="country">Country:</label>
             <input type="text" id="country" name="country">
 
             <label for="hiredate">Hire Date:</label>
             <input type="date" id="hiredate" name="hiredate" required>
+            <span id="hiredate-validation"  style="color: red;"></span>
+
             <label for="desig_id">Designation:</label>
             <select id="desig_id" name="desig_id" required>
                 <option value="Designer">Designer</option>
-                <option value="Coder">Coder</option>
+                <option value="Codder">Coder</option>
                 <option value="Tester">Tester</option>
             </select>
+            <label for="password"> Password:</label>
+            <input type="password" id="password" name="password" required>
 
             <button type="submit">Add Employee</button>
+            
         </form>
     </div>
-
-    <script>
-        const emailInput = document.getElementById('email');
-        const fnameInput = document.getElementById('fname');
-        const lnameInput = document.getElementById('lname');
-        const emailValidation = document.getElementById('email-validation');
-        const contactInput = document.getElementById('contact');
-        const contactValidation = document.getElementById('contact-validation');
-
-        emailInput.addEventListener('input', validateEmail);
-        fnameInput.addEventListener('input', validateName);
-        lnameInput.addEventListener('input', validatelName);
-        contactInput.addEventListener('input', validateContact);
-
-        function validateEmail() {
-            const email = emailInput.value;
-            const emailPattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
-
-            if (!email.match(emailPattern)) {
-                emailValidation.textContent = 'Please enter a valid email address.';
-            } else {
-                emailValidation.textContent = '';
-            }
-        }
-
-        function validateContact() {
-            const contact = contactInput.value;
-            const indianContactPattern = /^\d{10}$/;
-
-            if (!indianContactPattern.test(contact)) {
-                contactValidation.textContent = 'Contact number must be 10 digits and follow the Indian format.';
-            } else {
-                contactValidation.textContent = '';
-            }
-        }
-
-        function validateName() {
-    const fname = fnameInput.value.trim();
-    const fnameError = document.getElementById('fnameError');
-
-    // Check for leading or trailing spaces
-    if (fname !== fnameInput.value) {
-        fnameError.textContent = 'Name cannot have leading or trailing spaces.';
-    } else {
-        const words = fname.split(' ');
-
-        // Check if each part is at least 4 characters long, doesn't contain numbers, and doesn't exceed 20 characters
-        const invalidPart = words.find(part => part.length < 1 || /\d/.test(part) || part.length > 20);
-
-        if (fname === '') {
-            fnameError.textContent = 'Please enter a name.';
-        } else if (invalidPart) {
-            fnameError.textContent = 'Each word must be at least 4 characters long, not contain numbers, and not exceed 20 characters.';
-        } else {
-            fnameError.textContent = '';
-        }
-    }
-}
-
-function validatelName() {
-    const lname = lnameInput.value.trim();
-    const lnameError = document.getElementById('lnameError');
-
-    // Check for leading or trailing spaces
-    if (lname !== lnameInput.value) {
-        lnameError.textContent = 'Name cannot have leading or trailing spaces.';
-    } else {
-        const words = lname.split(' ');
-
-        // Check if each part is at least 4 characters long, doesn't contain numbers, and doesn't exceed 20 characters
-        const invalidPart = words.find(part => part.length < 1 || /\d/.test(part) || part.length > 20);
-
-        if (lname === '') {
-            lnameError.textContent = 'Please enter a name.';
-        } else if (invalidPart) {
-            lnameError.textContent = 'Each word must be at least 4 characters long, not contain numbers, and not exceed 20 characters.';
-        } else {
-            lnameError.textContent = '';
-        }
-    }
-}
-    </script>
+    <script src="validate.js"></script>
 </body>
 </html>
